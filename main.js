5e101bfb789018702b6bcce31d69cdafebd0e2a7
Ashok Parasa <aparasa@calpoly.edu>
2019-05-23 16:44:19 -0700

    Added State and Designation selection

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ let data;
 
 function getData()
 {
-    let requestURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + document.getElementById("state").value + "&api_key=wkSxIB6zEvqQeML5MiJwyulZzR0gegiZKOcmwxNc";
+    let requestURL = "https://developer.nps.gov/api/v1/parks?";
+    let apiKey = "api_key=wkSxIB6zEvqQeML5MiJwyulZzR0gegiZKOcmwxNc";
+    let state = document.getElementById("state").value;
+    if (state != "") //Filters by state if a state filter is selected
+    {
+        requestURL += "stateCode=" + state + "&"
+    }
+    requestURL += apiKey;
+    
     let request = new XMLHttpRequest();
     console.log("got URL");
     request.open('GET', requestURL);
@@ -19,13 +27,17 @@ function populateData()
 {
     let section = document.getElementById("info");
     let paraData = document.createElement("p");
-    let numResults = Number(data['total']);
+    //let numResults = Number(data['total']);
 
-    for (let i = 0; i < numResults; i++)
+    for (let i = 0; i < data['data'].length; i++)
     {
         let entry = data['data'][i];
-        paraData.appendChild(document.createTextNode(entry['fullName']));
-        paraData.appendChild(document.createElement("br"));
+        if (entry.hasOwnProperty('fullName'))
+        {
+            paraData.appendChild(document.createTextNode(entry['fullName']));
+            paraData.appendChild(document.createElement("br"));
+        }
+        
     }
 
     section.replaceChild(paraData, section.children[0]);
